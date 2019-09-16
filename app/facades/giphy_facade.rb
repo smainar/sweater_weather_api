8d3f52d0e9692a3b5bfce95f9a6a265432b43a19
Stella Mainar <mainars09@alumni.hanover.edu>
2019-09-16 10:11:58 -0600

    Work in progress - Faraday connection to Giphy

--- app/facades/giphy_facade.rb
+++ app/facades/giphy_facade.rb
@@ -1,8 +1,21 @@
 class GiphyFacade
-  attr_reader :location, :id
-  
+  attr_reader :location, :id, :query
+
   def initialize(location)
     @location = location
     @id = id
+    @query = query
+    @daily_weather = daily_weather
+  end
+
+# api.giphy.com/v1/gifs/search?api_key=bZlbEtbdYXEX7rMHU5Kk0ofuoUqFXIqv&q=partly_cloudy&limit=5
+  def get_giphy
+    giphy_conn = Faraday.new(url: "api.giphy.com/v1/gifs/search") do |f|
+      f.params["api_key"] = ENV["GIPHY-API-KEY"]
+      f.params["q"] = @location
+      f.params["limit"] = 5
+      f.adapter Faraday.default_adapter
+    end
+    require "pry"; binding.pry
   end
 end
