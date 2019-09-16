2a8dc39598d9495dcd75892258ec62fe2630934b
Stella Mainar <mainars09@alumni.hanover.edu>
2019-09-16 17:20:49 -0600

    Delete merged file from final_assessment branch: GiphyController

--- app/controllers/api/v1/giphy_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::GiphyController < ApplicationController
-  def index
-    # location = GiphyFacade.new(params[:location])
-    # render json: GiphySerializer.new(location).get_giphy
-    render json = GiphyFacade.new(params[:location]).get_giphy
-  end
-end
-# Pass weather data from Dark Sky's API, to return animated GIFs
-# Take raw giphy data and format through serializer
