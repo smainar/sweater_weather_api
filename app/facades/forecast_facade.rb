4b9c4218f8960695d420ca5872ca604f619f0262
Stella Mainar <mainars09@alumni.hanover.edu>
2019-09-16 17:05:54 -0600

    Refactor ForecastFacade to have public methods: current_weather, hourly_weather, daily_weather & private_methods: geocode_service, get_location_query_coordinates, get_formatted_lat_lng_coordinates, darksky_service, get_darksky_forecast_data

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -6,61 +6,41 @@ class ForecastFacade
     @location = location
   end
 
-  def geocode_conn
-    Faraday.new(url: "https://maps.googleapis.com") do |f|
-      f.params["address"] = location
-      f.params["key"] = ENV["GOOGLE-GEOCODING-API"]
-      f.adapter Faraday.default_adapter
-    end
-  end
-
-  def geocode_response
-    geocode_conn.get("/maps/api/geocode/json?address=#{location}&key=#{ENV["GOOGLE-GEOCODING-API"]}")
-  end
-
-  def google_geocode_raw_hash_data
-    JSON.parse(geocode_response.body, symbolize_names: true)
-  end
-
-  def google_geocode_query_coordinates
-    google_geocode_raw_hash_data[:results][0][:geometry][:location]
-  end
-
-  def latitude
-    google_geocode_query_coordinates[:lat]
-  end
-
-  def longitude
-    google_geocode_query_coordinates[:lng]
-  end
-
-  def darksky_conn
-    Faraday.new(url: "https://api.darksky.net") do |f|
-      f.adapter Faraday.default_adapter
-    end
-  end
-
-  def darksky_response
-    darksky_conn.get("/forecast/#{ENV["DARK-SKY-API"]}/#{latitude},#{longitude}")
-  end
-
-  def darksky_raw_hash_data
-    JSON.parse(darksky_response.body, symbolize_names: true)
-  end
-
   def current_weather
-    CurrentWeather.new(darksky_raw_hash_data[:currently])
+    CurrentWeather.new(get_darksky_forecast_data[:currently])
   end
 
   def hourly_weather
-    darksky_raw_hash_data[:hourly][:data].map do |hour|
+    get_darksky_forecast_data[:hourly][:data].map do |hour|
       HourlyWeather.new(hour)
     end.first(8)
   end
 
   def daily_weather
-    darksky_raw_hash_data[:daily][:data].map do |day|
+    get_darksky_forecast_data[:daily][:data].map do |day|
       DailyWeather.new(day)
     end.first(5)
+  end
+
+  private
+
+  def geocode_service
+    @geocode_service ||= GoogleGeocodeService.new(@location)
+  end
+
+  def get_location_query_coordinates
+    @get_location_query_coordinates ||= geocode_service.google_geocode_query_coordinates
+  end
+
+  def get_formatted_lat_lng_coordinates
+    "#{get_location_query_coordinates[:lat]},#{get_location_query_coordinates[:lng]}"
+  end
+
+  def darksky_service
+    @darksky_service ||= DarkskyService.new(get_formatted_lat_lng_coordinates)
+  end
+
+  def get_darksky_forecast_data
+    @get_darksky_forecast_data ||= darksky_service.darksky_raw_hash_data
   end
 end
