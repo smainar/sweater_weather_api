3fd98cf9444b02a2ea2c0f40586162ccdfc6573b
Stella Mainar <mainars09@alumni.hanover.edu>
2019-09-16 11:02:47 -0600

    Update index method in Api::V1::GiphyController

--- app/controllers/api/v1/giphy_controller.rb
+++ app/controllers/api/v1/giphy_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::GiphyController < ApplicationController
   def index
-    weather = GiphyFacade.new(params[:location])
-    render json: GiphySerializer.new(weather)
+    # location = GiphyFacade.new(params[:location])
+    # render json: GiphySerializer.new(location).get_giphy
+    render json = GiphyFacade.new(params[:location]).get_giphy
   end
 end
 # Pass weather data from Dark Sky's API, to return animated GIFs
