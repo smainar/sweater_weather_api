ddf8488303c0dd0a0c0752a07232b85eb4bb15e0
Stella Mainar <mainars09@alumni.hanover.edu>
2019-09-16 10:54:25 -0600

    Append 'https://' to url in line 11 for Faraday connection

--- app/facades/giphy_facade.rb
+++ app/facades/giphy_facade.rb
@@ -1,21 +1,18 @@
 class GiphyFacade
-  attr_reader :location, :id, :query
+  attr_reader :location, :id
 
   def initialize(location)
     @location = location
     @id = id
-    @query = query
-    @daily_weather = daily_weather
   end
 
 # api.giphy.com/v1/gifs/search?api_key=bZlbEtbdYXEX7rMHU5Kk0ofuoUqFXIqv&q=partly_cloudy&limit=5
-  def get_giphy
-    giphy_conn = Faraday.new(url: "api.giphy.com/v1/gifs/search") do |f|
-      f.params["api_key"] = ENV["GIPHY-API-KEY"]
-      f.params["q"] = @location
-      f.params["limit"] = 5
-      f.adapter Faraday.default_adapter
-    end
-    require "pry"; binding.pry
+   def get_giphy
+     giphy_conn = Faraday.new(url: "https://api.giphy.com/v1/gifs/search") do |f|
+       f.params["api_key"] = ENV["GIPHY-API-KEY"]
+       f.params["q"] = location
+       f.params["limit"] = 5
+       f.adapter Faraday.default_adapter
+     end
   end
 end
