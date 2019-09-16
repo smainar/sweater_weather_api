91262818dfff12542f3865672faed36415678285
Stella Mainar <mainars09@alumni.hanover.edu>
2019-09-16 11:34:48 -0600

    Pass in daily_weather_summary into Giphy cpnnect for query string

--- app/facades/giphy_facade.rb
+++ app/facades/giphy_facade.rb
@@ -35,15 +35,17 @@ class GiphyFacade
 
    darksky_raw_hash_data = JSON.parse(darksky_response.body, symbolize_names: true)
 
+   daily_weather_summary = darksky_raw_hash_data[:daily][:data][0][:summary]
+
    # Giphy Connection
    giphy_conn = Faraday.new(url: "https://api.giphy.com/v1/gifs/search") do |f|
      f.params["api_key"] = ENV["GIPHY-API-KEY"]
-     f.params["q"] = location
+     f.params["q"] = daily_weather_summary
      f.params["limit"] = 5
      f.adapter Faraday.default_adapter
    end
 
-   giphy_response = giphy_conn.get("?api_key=#{ENV["GIPHY-API-KEY"]}&q=#{@location}&limit=5")
+   giphy_response = giphy_conn.get("?api_key=#{ENV["GIPHY-API-KEY"]}&q=#{daily_weather_summary}&limit=5")
 
    giphy_raw_hash_data = JSON.parse(giphy_response.body, symbolize_names: true)
 
