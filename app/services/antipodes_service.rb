59e7933dc816f05d899a1e4f8ecdaeada56f36a7
Stella Mainar <mainars09@alumni.hanover.edu>
2019-09-17 11:55:40 -0600

    Add method (get_formatted_lat_lng_coordinates_to_pass_to_geocode_service) to AntipodesService & update antipode_response method

--- app/services/antipodes_service.rb
+++ app/services/antipodes_service.rb
@@ -5,6 +5,14 @@ class AntipodesService
 
   # http://amypode.herokuapp.com/api/v1/antipodes?lat=22.3193039&long=114.1693611
 
+  def get_formatted_lat_lng_coordinates_to_pass_to_geocode_service
+    "#{antipode_query_coordinates[:lat]},#{antipode_query_coordinates[:long]}"
+  end
+
+  def antipode_query_coordinates
+    antipode_raw_hash_data[:data][:attributes]
+  end
+
   def antipode_conn
     Faraday.new(url: "http://amypode.herokuapp.com") do |f|
       f.headers["api_key"] = ENV["ANTIPODE-API-KEY"]
@@ -13,7 +21,8 @@ class AntipodesService
   end
 
   def antipode_response
-    antipode_conn.get("/api/v1/antipodes?lat=22.3193039&long=114.1693611")
+    # antipode_conn.get("/api/v1/antipodes?lat=22.3193039&long=114.1693611")
+    antipode_conn.get("/api/v1/antipodes?lat=#{@coordinates[:lat]}&long=#{@coordinates[:lng]}")
   end
 
   def antipode_raw_hash_data
